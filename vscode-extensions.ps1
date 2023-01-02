4232868612057e10af2863d6fcbbc13db7d61c23
Quasar27 <38287385+QuasarIIVII@users.noreply.github.com>
2023-01-02 12:54:22 +0900

    230102A

---
+++ vscode-extensions.ps1
@@ -0,0 +1,7 @@
+code --install-extension christian-kohler.path-intellisense
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ritwickdey.LiveServer
+code --install-extension samplavigne.p5-vscode
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension VisualStudioExptTeam.vscodeintellicode
